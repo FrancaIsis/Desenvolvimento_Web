3d1aada17136721c47a26aecee3968676c3c263d
Isis França <166769666+FrancaIsis@users.noreply.github.com>
2025-05-15 16:25:54 -0300

    exercicio tabuada

---
+++ aula_javascript/atividade_001/ex011_tabuada/js/script.js
@@ -0,0 +1,32 @@
+// Carrega o HTML por completo
+document.addEventListener('DOMContentLoaded', function(){
+
+    
+    // receber a ul
+    const ulLista = document.getElementById('suaTabuada');
+
+
+
+    // função para limpar a lista
+    const limpar = () => ulLista.innerHTML = '';
+
+    // função para tabuada
+    document.getElementById('btnTabuada').addEventListener('click', function(){
+        // receber o elemento inserido no id 'numero' do html
+        const num = document.getElementById('numero').value.trim();
+        // transformando em inteiro
+        fator = parseInt(num);
+        for(let i=0; i<=9; i++){
+            const li = document.createElement('li');
+            li.textContent = fator * i;
+            ulLista.appendChild(li);
+        }
+        
+    });
+
+    // função para limpar 
+    document.getElementById('btnLimpar').addEventListener('click', function(){
+        limpar();
+    });
+    
+});
