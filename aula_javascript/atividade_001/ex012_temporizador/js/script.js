8feb2aa3303dd51fcb22d0f81d452426ac601614
Isis França <166769666+FrancaIsis@users.noreply.github.com>
2025-05-15 16:51:03 -0300

    inicio temporizador

---
+++ aula_javascript/atividade_001/ex012_temporizador/js/script.js
@@ -0,0 +1,7 @@
+// Carrega o HTML por completo
+document.addEventListener('DOMContentLoaded', function(){
+    //recebendo o contador do html
+    const cont = document.getElementById('contador');
+    let tempo = 10;
+
+});
