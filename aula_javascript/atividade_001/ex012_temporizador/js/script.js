730684d822e39c135e4bcb4471dcad6a22d88cef
Isis França <166769666+FrancaIsis@users.noreply.github.com>
2025-05-16 15:20:37 -0300

    temporizador

--- aula_javascript/atividade_001/ex012_temporizador/js/script.js
+++ aula_javascript/atividade_001/ex012_temporizador/js/script.js
@@ -3,5 +3,13 @@ document.addEventListener('DOMContentLoaded', function(){
     //recebendo o contador do html
     const cont = document.getElementById('contador');
     let tempo = 10;
-
+    const intervalo = setInterval(() => {
+      tempo--;//diminui o tempo
+      if (tempo >= 0) {
+        cont.textContent = tempo;// mostra o tempo
+      } else {
+        clearInterval(intervalo);//interrupção
+        cont.textContent = "Tempo esgotado!";
+      }
+    }, 1000);//set interval, executando a cada 1 segundo, entao essa função vai rodar a cada segundo
 });
