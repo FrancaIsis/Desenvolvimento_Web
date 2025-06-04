db3303cd2347e16933a5e24f6b63f7fa31518375
FrancaIsis <isisdej@outlokk.com>
2025-06-03 21:43:59 -0300

    atividade 03

---
+++ aula_javascript/atividade_003_arrays_filtros/js/script.js
@@ -0,0 +1,21 @@
+const produtos = [
+    {nome: 'Sabonete natural', valor: 15, categoria:'cosmetico'},
+    {nome: 'Shampoo orgânico', valor: 25, categoria:'cosmetico'},
+    {nome: 'Granola Artesanal', valor: 18, categoria:'alimento'},
+    {nome: 'Mel Puro', valor: 22, categoria:'alimento'},
+
+];
+
+// filtrar
+document.getElementById('categoria').addEventListener('change', function(){
+    const categoriaEscolhida = categoria.value;
+    const generoAlimenticio = categoriaEscolhida.filter(function(produtos){
+        return produtos.categoria == 'categoria';
+    });
+
+// exibindo o resultado
+    document.getElementById('resultado-filtrar').innerHTML = `
+    <strong>${generoAlimenticio}</strong>
+    `;
+    
+});
